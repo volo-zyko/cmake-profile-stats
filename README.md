1647e6c12d997ebd7034e1624be7dce67c2b446d
Volo Zyko <volo.zyko@gmail.com>
2020-04-05 00:03:03 +0300

    Remove cmake patch and switch to cmake 3.17
    The new cmake provides timestamp information out of the box, hence
    no need for a patch. Kudos to @fwinkl.
    Also cleanup the code and improve readability.

--- README.md
+++ README.md
@@ -2,53 +2,50 @@
 
 This small Python script collects information about the time spent in each call within cmake script and prints nice stack traces sorted according to timing of each call.
 
-Although currently cmake's trace does not provide timestamp information, here you can find a patch for cmake which adds timestamps for traced lines. With this information `cmake-profile-stats.py` reconstructs stack traces from plain log.
-
 Short help about usage of this script:
 
 ```
-usage: cmake-profile-stat.py [-h] [-f SHELVE_FILE] [-t THRESHOLD] [-d DEPTH]
-                             [--ignore-nesting] [-w TRACE_INFO_WIDTH] [-s]
-                             [-r] [-1] [-v]
+usage: cmake-profile-stat.py [-h] [-f SHELF_FILE] [-r] [-t THRESHOLD]
+                             [-d DEPTH] [--ignore-nesting]
+                             [-w TRACE_INFO_WIDTH] [-s] [-1]
                              [trace]
 
-Process cmake execution log produced with --trace/--trace-expand command line
-options.
-Note: In order to provide command's timestamps CMake should be patched with
-either of the diffs provided alongside this script and compiled from source
+Process cmake execution log produced with --trace-format=json-v1 command
+line argument.
 
 positional arguments:
   trace                 cmake trace log or stdin
 
 optional arguments:
   -h, --help            show this help message and exit
-  -f SHELVE_FILE, --shelve-file SHELVE_FILE
-                        file for shelf container, which is used in subsequent
-                        script runs without recurring log processing {default:
-                        cmake.traces}
+  -f SHELF_FILE, --shelf-file SHELF_FILE
+                        collect stats to the provided shelf file (without
+                        printing a report) {default: cmake.traces}
+  -r, --report-only     do not collect stats, make a report from the shelved
+                        stats instead (stats specified with --shelf-file)
+                        {default: False}
   -t THRESHOLD, --threshold THRESHOLD
                         do not report traces with relative time lower than the
-                        threshold, for example 0.01 corresponds to 1% of the
+                        threshold; for example, 0.01 corresponds to 1% of the
                         whole execution time {default: 0}
   -d DEPTH, --depth DEPTH
-                        do not report traces with depth bigger than requested
+                        do not report traces with depth greater than requested
                         (depth=0 is ignored) {default: 0}
-  --ignore-nesting      ignore nesting level field in input cmake log
+  --ignore-nesting      ignore "frame" field provided by cmake and build up
+                        "scopes" nesting based on the file names in order of
+                        cmake code execution
   -w TRACE_INFO_WIDTH, --trace-info-width TRACE_INFO_WIDTH
                         fixed width in characters of a variable part of cmake
                         trace (file name, line number, nesting) in generated
                         report {default: None}
   -s, --sort-traces     sort subcalls in a trace according to their timings
                         {default: False}
-  -r, --report-only     do not collect stats, make a report from shelved stats
-                        instead {default: False}
   -1, --one             report only the most expensive stack trace {default:
                         False}
-  -v, --verbose         enable verbose output {default: False}
 
-Each trace line in a report has 6 distinct parts:
+Each trace line in the resulting report has 6 distinct parts:
 1) nesting level in square brackets;
-2) path to original cmake script (maybe with dots in the middle and dots at
+2) path to the original cmake script (maybe with dots in the middle and dots at
    the end if -w option was used);
 3) line number of a traced line in the original cmake script in parentheses;
 4) a line of code as it was traced by cmake;
--- cmake-profile-stat.py
+++ cmake-profile-stat.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import argparse
 import collections
+import json
 import os
 import re
 import shelve
@@ -18,14 +19,12 @@ def _process_arguments():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description='''
-Process cmake execution log produced with --trace/--trace-expand command line
-options.
-Note: In order to provide command's timestamps CMake should be patched with
-either of the diffs provided alongside this script and compiled from source''',
+Process cmake execution log produced with --trace-format=json-v1 command
+line argument.''',
         epilog=textwrap.dedent('''
-Each trace line in a report has 6 distinct parts:
+Each trace line in the resulting report has 6 distinct parts:
 1) nesting level in square brackets;
-2) path to original cmake script (maybe with dots in the middle and dots at
+2) path to the original cmake script (maybe with dots in the middle and dots at
    the end if -w option was used);
 3) line number of a traced line in the original cmake script in parentheses;
 4) a line of code as it was traced by cmake;
@@ -41,24 +40,30 @@ script's messages and this script outputs those lines starting with
 "Ignored: " string.'''))
 
     parser.add_argument(
-        'trace', nargs='?', default=None,
+        'trace', nargs='?', type=argparse.FileType('r'), default=sys.stdin,
         help='cmake trace log or stdin')
     parser.add_argument(
-        '-f', '--shelve-file', default='cmake.traces',
-        help='file for shelf container, which is used in subsequent script '
-             'runs without recurring log processing {default: %(default)s}')
+        '-f', '--shelf-file', default='cmake.traces',
+        help='collect stats to the provided shelf file (without printing '
+             'a report) {default: %(default)s}')
+    parser.add_argument(
+        '-r', '--report-only', action='store_true',
+        help='do not collect stats, make a report from the shelved stats '
+             'instead (stats specified with --shelf-file) '
+             '{default: %(default)s}')
     parser.add_argument(
         '-t', '--threshold', default=0, type=float,
         help='do not report traces with relative time lower than the '
-             'threshold, for example 0.01 corresponds to 1%% of the whole '
+             'threshold; for example, 0.01 corresponds to 1%% of the whole '
              'execution time {default: %(default)s}')
     parser.add_argument(
         '-d', '--depth', default=0, type=int,
-        help='do not report traces with depth bigger than requested (depth=0 '
+        help='do not report traces with depth greater than requested (depth=0 '
              'is ignored) {default: %(default)s}')
     parser.add_argument(
         '--ignore-nesting', action='store_true',
-        help='ignore nesting level field in input cmake log')
+        help='ignore "frame" field provided by cmake and build up "scopes" '
+             'nesting based on the file names in order of cmake code execution')
     parser.add_argument(
         '-w', '--trace-info-width', default=None, type=int,
         help='fixed width in characters of a variable part of cmake trace '
@@ -69,16 +74,9 @@ script's messages and this script outputs those lines starting with
         help='sort subcalls in a trace according to their timings '
              '{default: %(default)s}')
     parser.add_argument(
-        '-r', '--report-only', action='store_true',
-        help='do not collect stats, make a report from shelved stats instead '
-             '{default: %(default)s}')
-    parser.add_argument(
         '-1', '--one', action='store_true',
         help='report only the most expensive stack trace '
              '{default: %(default)s}')
-    parser.add_argument(
-        '-v', '--verbose', action='store_true',
-        help='enable verbose output {default: %(default)s}')
 
     return parser.parse_args()
 
@@ -195,59 +193,38 @@ def _update_traces(traces, parent_trace, current_timeval,
 
 
 def _parse_cmake_log(file_obj, ignore_nesting):
-    matcher = re.compile(r'^\((?P<timestamp>[^)]*)\)\s*'
-                         r'(\((?P<frame>[^)]*)\)\s*)?'
-                         r'(?P<file>[^(]*)\((?P<line>[^)]*)\):\s*'
-                         r'(?P<code>.*)$')
+    def build_cmake_command(trace_item):
+        return '{0}({1}{2}{1})'.format(trace_item['cmd'],
+                                       '"' if trace_item['args'] else '',
+                                       '" "'.join(trace_item['args']))
 
-    def match_parens(code):
-        nesting = 0
-        for c in code:
-            if c == '(':
-                nesting += 1
-            elif c == ')':
-                nesting -= 1
-        return nesting == 0
-
-    # In cmake 3.10 at least `else` and `elseif` commands decrease nesting
+    # In cmake 3.17, at least `else` and `elseif` commands decrease nesting
     # level while they should not modify it at all. Once encounter such
     # commands we workaround this behavior increasing nesting level by 1.
-    cmake_commands_with_nesting_bugs_re = re.compile(r'^(else|elseif)\s*\(')
+    commands_with_nesting_bugs = ('else', 'elseif')
 
     current_nesting = None
     current_timeval = None
     current_trace_info = None
     for line in file_obj:
-        # Strip possible /r on Windows.
-        line = line.rstrip()
+        try:
+            trace_item = json.loads(line, encoding='utf-8')
 
-        match = matcher.match(line)
-        if match is None:
-            if (current_trace_info is None or
-                    match_parens(current_trace_info.cmake_code_line)):
-                # We log to stderr all lines which don't match.
-                print('Ignored: {}'.format(line), file=sys.stderr)
-            else:
-                current_trace_info.cmake_code_line += '\\n{}'.format(line)
+            current_timeval = float(trace_item['time'])
+            cmake_code = build_cmake_command(trace_item)
+            current_trace_info = _CmakeTraceInfo(trace_item['file'],
+                                                 int(trace_item['line']),
+                                                 cmake_code)
+
+            if not ignore_nesting:
+                current_nesting = int(trace_item['frame'])
+                if trace_item['cmd'] in commands_with_nesting_bugs:
+                    current_nesting += 1
+        except Exception:
+            print('Ignored: {}'.format(line), file=sys.stderr, end='')
             continue
 
-        if current_trace_info is not None:
-            yield (current_nesting, current_timeval, current_trace_info)
-
-        cmake_code = match.group('code')
-
-        if not ignore_nesting:
-            current_nesting = match.group('frame')
-            if current_nesting is not None:
-                current_nesting = int(current_nesting)
-                # Workaround cmake bugs.
-                if cmake_commands_with_nesting_bugs_re.search(cmake_code):
-                    current_nesting += 1
-
-        current_timeval = float(match.group('timestamp'))
-        current_trace_info = _CmakeTraceInfo(match.group('file'),
-                                             int(match.group('line')),
-                                             cmake_code)
+        yield (current_nesting, current_timeval, current_trace_info)
     else:
         if current_trace_info is not None:
             yield (current_nesting, current_timeval, current_trace_info)
@@ -292,15 +269,13 @@ def _collect_stats(traces, file_obj, ignore_nesting):
 
 def _print_traces(args, ti_to_string, all_traces, whole_duration):
     def print_traces_loop(traces, indent):
-        ordered_traces = traces
         if args.sort_traces:
-            ordered_traces = sorted(traces, key=lambda x: x.duration,
-                                    reverse=True)
+            traces = sorted(traces, key=lambda x: x.duration, reverse=True)
 
-        for trace in ordered_traces:
+        for trace in traces:
             if args.depth and indent + 1 > args.depth:
                 break
-            if trace.duration / whole_duration < args.threshold:
+            elif trace.duration / whole_duration < args.threshold:
                 break
 
             ti_str = ti_to_string(trace.trace_info,
@@ -321,38 +296,34 @@ def _print_traces(args, ti_to_string, all_traces, whole_duration):
 
 _StoredTrace = collections.namedtuple('_StoredTrace',
                                       ['trace_info', 'duration', 'subtraces'])
+_TRACES_KEY = 'traces'
 
 
 def _main(args):
-    traces_key = 'traces'
+    if os.path.exists(args.shelf_file) and not args.report_only:
+        os.remove(args.shelf_file)
 
-    input_stream = sys.stdin
-    if args.trace is not None:
-        input_stream = open(args.trace)
+    traces_store = shelve.open(args.shelf_file)
 
-    if os.path.exists(args.shelve_file) and not args.report_only:
-        os.remove(args.shelve_file)
-
-    traces_store = shelve.open(args.shelve_file)
-
+    all_traces = []
     try:
         if args.report_only:
-            all_traces = traces_store.get(traces_key, [])
+            all_traces = traces_store.get(_TRACES_KEY, [])
         else:
-            all_traces = []
-            _collect_stats(all_traces, input_stream, args.ignore_nesting)
+            _collect_stats(all_traces, args.trace, args.ignore_nesting)
 
             def store_trace(trace):
                 return _StoredTrace(trace.trace_info, trace.duration,
                                     [store_trace(t) for t in trace.subtraces])
 
-            traces_store[traces_key] = [store_trace(t) for t in all_traces]
-        traces_store.close()
+            traces_store[_TRACES_KEY] = [store_trace(t) for t in all_traces]
     except:
         traces_store.close()
-        if os.path.exists(args.shelve_file):
-            os.remove(args.shelve_file)
+        if os.path.exists(args.shelf_file):
+            os.remove(args.shelf_file)
         raise
+    else:
+        traces_store.close()
 
     whole_duration = sum(t.duration for t in all_traces)
 
